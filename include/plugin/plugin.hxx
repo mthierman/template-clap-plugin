936e113c6fc90ed677827017ab191e223aa70f9f
mthierman <mthierman@gmail.com>
2024-07-27 14:47:23 -0600

    PluginFactory

--- include/plugin/plugin.hxx
+++ include/plugin/plugin.hxx
@@ -2,6 +2,7 @@
 #include <clap/helpers/plugin.hxx>
 
 #include <array>
+#include <functional>
 #include <initializer_list>
 
 using PluginHelper = clap::helpers::Plugin<clap::helpers::MisbehaviourHandler::Terminate,
@@ -18,4 +19,39 @@ template <typename T> constexpr auto makePluginDescriptor(T features) -> clap_pl
              .version { PLUGIN_VERSION },
              .description { PLUGIN_DESCRIPTION },
              .features { features } };
+}
+
+struct PluginFactory {
+    static auto getPluginCount(const clap_plugin_factory* factory) -> uint32_t {
+        return getPluginCountCallback(factory);
+    }
+
+    static auto getPluginDescriptor(const clap_plugin_factory* factory,
+                                    uint32_t index) -> const clap_plugin_descriptor* {
+        return getPluginDescriptorCallback(factory, index);
+    }
+
+    static auto createPlugin(const struct clap_plugin_factory* factory,
+                             const clap_host_t* host,
+                             const char* plugin_id) -> const clap_plugin* {
+        return createPluginCallback(factory, host, plugin_id);
+    }
+
+    static std::function<uint32_t(const clap_plugin_factory* factory)> getPluginCountCallback;
+    static std::function<const clap_plugin_descriptor*(const struct clap_plugin_factory* factory,
+                                                       uint32_t index)>
+        getPluginDescriptorCallback;
+    static std::function<const clap_plugin*(
+        const struct clap_plugin_factory* factory, const clap_host_t* host, const char* plugin_id)>
+        createPluginCallback;
+};
+
+constexpr auto makePluginFactory() -> clap_plugin_factory {
+    clap_plugin_factory pluginFactory {
+        .get_plugin_count { PluginFactory::getPluginCount },
+        .get_plugin_descriptor { PluginFactory::getPluginDescriptor },
+        .create_plugin { PluginFactory::createPlugin },
+    };
+
+    return pluginFactory;
 }
