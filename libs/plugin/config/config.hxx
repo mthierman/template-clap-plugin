890d546439510cdda9f90779b3c7b666ddbe2ce9
mthierman <mthierman@gmail.com>
2024-07-29 16:01:01 -0600

    Fix config spaces

--- libs/plugin/config/config.hxx
+++ libs/plugin/config/config.hxx
@@ -1,5 +1,5 @@
 // clang-format off
 #include <array>
 
-constexpr std::array features { @PLUGIN_FEATURES@,static_cast<const char*>(nullptr) };
+constexpr std::array features { @PLUGIN_FEATURES@, static_cast<const char*>(nullptr) };
 // clang-format on
