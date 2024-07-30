2014be780e4e063ee710a48fff97576c57576561
mthierman <mthierman@gmail.com>
2024-07-29 19:35:36 -0600

    Check for env before creating it

--- libs/plugin/include/plugin/win32.hxx
+++ libs/plugin/include/plugin/win32.hxx
@@ -8,47 +8,25 @@
 namespace plugin {
 struct Window final : glow::window::Window {
     Window() {
-        // auto ctx = ::GetThreadDpiAwarenessContext();
-        // auto aware = ::GetAwarenessFromDpiAwarenessContext(ctx);
-
-        // if (ctx == DPI_AWARENESS_CONTEXT_UNAWARE) {
-        //     glow::system::message_box("DPI_AWARENESS_CONTEXT_UNAWARE");
-        // }
-        // if (ctx == DPI_AWARENESS_CONTEXT_SYSTEM_AWARE) {
-        //     glow::system::message_box("DPI_AWARENESS_CONTEXT_SYSTEM_AWARE");
-        // }
-        // if (ctx == DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE) {
-        //     glow::system::message_box("DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE");
-        // }
-        // if (ctx == DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2) {
-        //     glow::system::message_box("DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2");
-        // }
-        // if (ctx == DPI_AWARENESS_CONTEXT_UNAWARE_GDISCALED) {
-        //     glow::system::message_box("DPI_AWARENESS_CONTEXT_UNAWARE_GDISCALED");
-        // }
-
-        // switch (aware) {
-        //     case DPI_AWARENESS_INVALID: {
-        //         glow::system::message_box("DPI_AWARENESS_INVALID");
-        //     } break;
-        //     case DPI_AWARENESS_UNAWARE: {
-        //         glow::system::message_box("DPI_AWARENESS_UNAWARE");
-        //     } break;
-        //     case DPI_AWARENESS_SYSTEM_AWARE: {
-        //         glow::system::message_box("DPI_AWARENESS_SYSTEM_AWARE");
-        //     } break;
-        //     case DPI_AWARENESS_PER_MONITOR_AWARE: {
-        //         glow::system::message_box("DPI_AWARENESS_PER_MONITOR_AWARE");
-        //     } break;
-        // }
-
         webViewEnvironment.m_userDataFolder
             = glow::filesystem::known_folder(FOLDERID_LocalAppData, { "template-clap-plugin" });
 
         message(WM_CREATE, [this](glow::messages::wm_create message) {
             glow::window::set_position(m_hwnd.get(), 0, 0, 640, 480);
 
-            webViewEnvironment.create([this]() {
+            if (!webViewEnvironment.m_environment) {
+                webViewEnvironment.create([this]() {
+                    webView.create(webViewEnvironment, m_hwnd.get(), [this]() {
+#if HOT_RELOAD
+                        webView.navigate(DEV_URL);
+#else
+                        webView.navigate("https://www.example.com/");
+#endif
+                        webView.put_bounds(m_hwnd.get());
+                    });
+                });
+            }
+            else {
                 webView.create(webViewEnvironment, m_hwnd.get(), [this]() {
 #if HOT_RELOAD
                     webView.navigate(DEV_URL);
@@ -57,7 +35,7 @@ struct Window final : glow::window::Window {
 #endif
                     webView.put_bounds(m_hwnd.get());
                 });
-            });
+            }
 
             return 0;
         });
